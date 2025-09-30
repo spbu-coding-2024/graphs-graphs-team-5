c00f0a15c29767cf8c10103f63282e9f32804d6f
TheFollan <a.a-t@bk.ru>
2025-09-30 03:23:32 +0300

    fixes ktlint

--- app/src/main/kotlin/boyaan/model/algorithms/classic/BridgesFind.kt
+++ app/src/main/kotlin/boyaan/model/algorithms/classic/BridgesFind.kt
@@ -1,30 +1,33 @@
 package boyaan.model.algorithms.classic
 
-import boyaan.model.core.base.Graph
 import boyaan.model.core.base.Edge
+import boyaan.model.core.base.Graph
 import boyaan.model.core.base.Vertex
 
 class BridgesFind<V, E> {
     private var timer = 0
 
-    fun bridgesFind(graph: Graph<V, E>) : List<Edge<E>> {
+    fun bridgesFind(graph: Graph<V, E>): List<Edge<E>> {
         val bridges = mutableListOf<Edge<E>>()
         val visited = mutableListOf<Vertex<V>>()
         val tin = mutableMapOf<Vertex<V>, Int>()
         val low = mutableMapOf<Vertex<V>, Int>()
 
-        fun dfs(u: Vertex<V>, parent: Vertex<V>?) {
+        fun dfs(
+            u: Vertex<V>,
+            parent: Vertex<V>?,
+        ) {
             visited.add(u)
             tin[u] = timer
             low[u] = timer
             timer++
 
-            for(v in neighbors(graph, u)) {
-                if(v == parent) continue
-                if (v !in visited){
+            for (v in neighbors(graph, u)) {
+                if (v == parent) continue
+                if (v !in visited) {
                     dfs(v, u)
                     low[u] = minOf(low[u]!!, low[v]!!)
-                    if(low[v]!! > tin[u]!!) {
+                    if (low[v]!! > tin[u]!!) {
                         graph[u.key, v.key]?.let { bridges.add(it) }
                     }
                 } else {
@@ -33,20 +36,22 @@ class BridgesFind<V, E> {
             }
         }
 
-        for(v in graph.vertices){
-            if(v !in visited) dfs(v, null)
+        for (v in graph.vertices) {
+            if (v !in visited) dfs(v, null)
         }
 
         return bridges
     }
 
-    private fun neighbors(graph: Graph<V, E>, vertex: Vertex<V>): List<Vertex<V>> {
-        return graph.edges.mapNotNull { edge ->
-            when(vertex.key){
+    private fun neighbors(
+        graph: Graph<V, E>,
+        vertex: Vertex<V>,
+    ): List<Vertex<V>> =
+        graph.edges.mapNotNull { edge ->
+            when (vertex.key) {
                 edge.key.first -> graph[edge.key.second]
                 edge.key.second -> graph[edge.key.first]
                 else -> null
             }
         }
-    }
 }
