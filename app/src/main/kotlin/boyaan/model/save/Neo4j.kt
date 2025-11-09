dc3539f8c169faf1c04f5524d760eb565d0b4e20
TheFollan <a.a-t@bk.ru>
2025-11-09 20:12:44 +0300

    minor fixes

--- app/src/main/kotlin/boyaan/model/save/Neo4j.kt
+++ app/src/main/kotlin/boyaan/model/save/Neo4j.kt
@@ -7,12 +7,18 @@ import org.neo4j.driver.AuthTokens
 import org.neo4j.driver.Driver
 import org.neo4j.driver.GraphDatabase
 import java.io.Closeable
+import boyaan.model.core.internals.directedWeighted.DirectedWeightedGraph
+import boyaan.model.core.internals.weighted.UndirectedWeightedGraph
+import boyaan.model.core.internals.directed.DirectedUnweightedGraph
+
+enum class GraphT { DEFAULT, DIRECTED, WEIGHTED, DIRECTED_WEIGHTED }
 
 class Neo4j(
     uri: String,
     user: String,
     password: String,
 ) : Closeable {
+
     private val driver: Driver = GraphDatabase.driver(uri, AuthTokens.basic(user, password))
 
     fun <V, E> exportGraph(
@@ -21,14 +27,26 @@ class Neo4j(
         batchSize: Int = 500,
     ) {
         driver.session().use { session ->
-            session.executeWrite { tx ->
-                tx.run("CREATE CONSTRAINT IF NOT EXIST FOR (v:Vertex) REQUIRE v.index IS UNIQUE")
-            }
 
             if (clearDatabase) {
                 session.executeWrite { tx ->
                     tx.run("MATCH (n) DETACH DELETE n")
                 }
+            }
+            val graphType = when (graph) {
+                is DirectedWeightedGraph -> GraphT.DIRECTED_WEIGHTED.name
+                is DirectedUnweightedGraph -> GraphT.DIRECTED.name
+                is UndirectedWeightedGraph -> GraphT.WEIGHTED.name
+                else -> GraphT.DEFAULT.name
+            }
+            session.executeWrite { tx ->
+                tx.run(
+                    """
+                        MERGE (g:GraphInfo)
+                        SET g.type = \${'$'}type
+                        """.trimIndent(),
+                    mapOf("type" to graphType)
+                )
             }
 
             val vertexList = graph.vertices.toList()
@@ -36,7 +54,7 @@ class Neo4j(
 
             val nodesPayload =
                 vertexList.mapIndexed { index, vertex ->
-                    mapOf("index" to index, "value" to vertex.value.toString())
+                    mapOf("origId" to index, "value" to vertex.value.toString())
                 }
 
             val edgesPayload =
@@ -60,7 +78,7 @@ class Neo4j(
                     tx.run(
                         """
                         UNWIND $${"nodes"} AS nodeEntry
-                        MERGE (v:Vertex {origId: nodeEntry.index})
+                        MERGE (v:Vertex {origId: nodeEntry.origId})
                         SET v.value = nodeEntry.value
                         """.trimIndent(),
                         mapOf("nodes" to batch),
@@ -73,7 +91,7 @@ class Neo4j(
                     tx.run(
                         """
                         UNWIND $${"edges"} AS edgeEntry
-                        MATCH (a:Vertex {index: edgeEntry.uIndex}), (b:Vertex {index: edgeEntry.vIndex})
+                        MATCH (a:Vertex {origId: edgeEntry.uIndex}), (b:Vertex {origId: edgeEntry.vIndex})
                         MERGE (a)-[r:EDGE {value: edgeEntry.value}]->(b)
                         """.trimIndent(),
                         mapOf("edges" to batch),
@@ -87,12 +105,26 @@ class Neo4j(
         valueParser: (String) -> V,
         edgeParser: (String) -> E,
     ): Graph<V, E> {
-        val resultGraph = DefaultGraph<V, E>()
 
         driver.session().use { session ->
+            val graphType = session.executeRead { tx ->
+                val result = tx.run("MATCH (g:GraphInfo) RETURN g.type AS type").list()
+                if (result.isNotEmpty() && !result[0]["type"].isNull) {
+                    result[0]["type"].asString()
+                } else {
+                    GraphT.DEFAULT.name
+                }
+            }
+
+            val resultGraph: Graph<V, E> = when (GraphT.valueOf(graphType)) {
+                GraphT.DEFAULT -> DefaultGraph()
+                GraphT.DIRECTED -> DirectedUnweightedGraph()
+                GraphT.WEIGHTED -> UndirectedWeightedGraph()
+                GraphT.DIRECTED_WEIGHTED -> DirectedWeightedGraph()
+            }
             val vertexRecords =
                 session.executeRead { tx ->
-                    tx.run("MATCH (v:Vertex) RETURN v.index AS idx, v.value AS value ORDER BY v.index ASC").list()
+                    tx.run("MATCH (v:Vertex) RETURN v.origId AS idx, v.value AS value ORDER BY v.origId ASC").list()
                 }
 
             val indexToVertex = mutableMapOf<Int, Vertex<V>>()
@@ -108,7 +140,7 @@ class Neo4j(
                 session.executeRead { tx ->
                     tx
                         .run(
-                            "MATCH (a:Vertex)-[r:EDGE]->(b:Vertex) RETURN a.index AS uIndex, b.index AS vIndex, r.value AS value",
+                            "MATCH (a:Vertex)-[r:EDGE]->(b:Vertex) RETURN a.origId AS uIndex, b.origId AS vIndex, r.value AS value",
                         ).list()
                 }
 
@@ -125,9 +157,8 @@ class Neo4j(
                     resultGraph.addEdge(fromVertex.key, toVertex.key, parsedEdgeValue)
                 }
             }
+            return resultGraph
         }
-
-        return resultGraph
     }
 
     override fun close() {
